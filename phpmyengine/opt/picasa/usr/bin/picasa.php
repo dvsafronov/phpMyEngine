bf20cd6a626743fad7c9726da6e0d58f5bb7674a
Denis xmcdbx Safronov <mcdb@mcdb.ru>
2011-02-16 00:14:57 +0300

    2011.02.16@00:10

--- phpmyengine/opt/picasa/usr/bin/picasa.php
+++ phpmyengine/opt/picasa/usr/bin/picasa.php
@@ -4,27 +4,45 @@ namespace phpMyEngine\picasaController;
 \phpMyEngine\loadModule ( 'picasa' );
 
 function defaultAction () {
-    $myPicasa = new \phpMyEngine\Picasa\Picasa();
-
     $_myConfig = \phpMyEngine\Config\Config::getInstance ();
     $_myConfig->load ( 'picasa', true );
 
     if (strlen ( $_myConfig->opt->picasa->user ) > 1) {
-
         $_myRender = \phpMyEngine\Render\Render::getInstance();
+        $myPicasa = new \phpMyEngine\Picasa\Picasa();
         $myPicasa->user = $_myConfig->opt->picasa->user;
         $myPicasa->thumbWidth = (int) $_myConfig->opt->picasa->width;
-
         if (null != $_myConfig->opt->picasa->forceAlbum) {
             //грузим сразу альбом
             $imgs = $myPicasa->getImages ( $_myConfig->opt->picasa->forceAlbum );
             $_myRender->setValue ( 'album', $imgs );
-            $tpl = 'album';
+            $tpl = 'images';
         } else {
-            $albms = $myPicasa->getAlbums ();
+            $albums = $myPicasa->getAlbums ();
+            $_myRender->setValue ( 'albums', $albums );
+            $tpl = 'albums';
         }
         $_myRender->renderTemplate ( 'picasa/' . $tpl . '.tpl' );
     } else {
         \phpMyEngine\logError ( 'Picasa module not configured' );
     }
+}
+
+function viewAction () {
+    $_myRoute = \phpMyEngine\Route::getInstance ();
+    $_myRender = \phpMyEngine\Render\Render::getInstance();
+    $_myConfig = \phpMyEngine\Config\Config::getInstance ();
+    $_myConfig->load ( 'picasa', true );
+
+    $myPicasa = new \phpMyEngine\Picasa\Picasa();
+    $myPicasa->user = $_myConfig->opt->picasa->user;
+    $myPicasa->thumbWidth = (int) $_myConfig->opt->picasa->width;
+    if (null != $_myConfig->opt->picasa->forceAlbum) {
+        $imgs = $myPicasa->getImages ( $_myConfig->opt->picasa->forceAlbum );
+    } else {
+        $imgs = $myPicasa->getImages ( $_myRoute->id );
+    }
+
+    $_myRender->setValue ( 'album', $imgs );
+    $_myRender->renderTemplate ( 'picasa/images.tpl' );
 }
